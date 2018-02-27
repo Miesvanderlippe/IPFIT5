5c12e8a749e1dd354e97856df399f42d74df9c86
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-02-27 12:14:03 +0100

    Fixed the logger init in Modules

--- Modules/PhotoModule.py
+++ Modules/PhotoModule.py
@@ -5,3 +5,8 @@ class PhotoModule(ModuleInterface):
 
     def __init__(self):
         super().__init__()
+
+
+if __name__ == '__main__':
+    module = PhotoModule()
+    print(module.logger.name)  # PhotoModule
