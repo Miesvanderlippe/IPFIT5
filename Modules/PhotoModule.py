d4a576da88eb444817d60d2663e5f4006ccd20f0
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-02-28 11:53:07 +0100

    Added v. basic csv writer

--- Modules/PhotoModule.py
+++ Modules/PhotoModule.py
@@ -1,4 +1,7 @@
 from Interfaces.ModuleInterface import ModuleInterface
+from Utils.Store import Store
+from Utils.Ewf import Ewf
+import csv
 
 
 class PhotoModule(ModuleInterface):
@@ -6,7 +9,34 @@ class PhotoModule(ModuleInterface):
     def __init__(self) -> str:
         super().__init__()
 
+    @staticmethod
+    def write_csv(data, output):
+        if not data:
+            return
+
+        with open(output, 'w') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            headers = ['Partition', 'File', 'File Ext', 'File Type',
+                       'Create Date', 'Modify Date', 'Change Date', 'Size',
+                       'File Path', 'Hash']
+            csv_writer.writerow(headers)
+            for result_list in data:
+                csv_writer.writerows(result_list)
+
 
 if __name__ == '__main__':
     module = PhotoModule()
     print(module.logger.name)  # PhotoModule
+
+    store = Store()
+    store.image_store.dispatch(
+        {
+            'type': 'set_image',
+            'image': '/Users/Mies/Downloads/HFS.dd'
+        }
+    )
+
+    ewf = Ewf()
+    data = ewf.files()
+    PhotoModule.write_csv(data, "files.csv")
+
