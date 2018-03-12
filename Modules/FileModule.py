893b4728bd2f2fc9db1d43db2ea27729a7fa003b
Hendrik Brouwer <Hendrik250@kpnmail.nl>
2018-03-12 14:46:59 +0100

    Lijst van bestanden uit de .zip/.tar.gz bestanden wegschrijven naar .csv

--- Modules/FileModule.py
+++ Modules/FileModule.py
@@ -1,15 +1,18 @@
 from Interfaces.ModuleInterface import ModuleInterface
 import zipfile
 import tarfile
-
+import csv
+import os
 
 class FileModule(ModuleInterface):
 
     def __init__(self) -> None:
         super().__init__()
 
+# aanmaken lijst waar bestandsnamen die zich in de containers bevinden naar worden weggeschreven
 archievenlijst = []
 
+# uitlezen van de zip archieven, deze vervolgens appenden aan de archievenlijst
 def zips_uitlezen():
     zips = zipfile.ZipFile("ziparchief.zip",'r')
    # print(zips.namelist)
@@ -17,13 +20,18 @@ def zips_uitlezen():
         archievenlijst.append(item)
     # print(archievenlijst)
 
+# uitlezen van de tar archieven, deze vervolgens appenden aan de archievenlijst
 def tars_uitlezen():
     tars = tarfile.open("tararchief.tar.gz", "r:gz")
     for tarinfo in tars:
         archievenlijst.append(tarinfo.name)
-    print(archievenlijst)
+    # print(archievenlijst)
 
-
+# Alle bestandsnamen wegschrijven naar een .csv op een nieuwe regel.
+def archieflijst_wegschrijven():
+    with open("archiefbestanden.csv", "w") as f:
+        writer = csv.writer(f, delimiter="\n")
+        writer.writerow(archievenlijst)
 
 
 
@@ -31,5 +39,5 @@ def tars_uitlezen():
 if __name__ == '__main__':
     zips_uitlezen()
     tars_uitlezen()
-
+    archieflijst_wegschrijven()
 
