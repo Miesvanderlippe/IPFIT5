a9d7c59650f10d3bea3a900b37a078dc4c01c54c
Hendrik Brouwer <Hendrik250@kpnmail.nl>
2018-03-16 14:01:57 +0100

    langdetect toegevoegd aan requirements.txt, tar en zip archieven uitlezen gecombineerd tot 1 functie

--- Modules/FileModule.py
+++ Modules/FileModule.py
@@ -5,8 +5,6 @@ import csv
 import os
 from langdetect import detect
 
-
-
 class FileModule(ModuleInterface):
 
     def __init__(self) -> None:
@@ -16,39 +14,41 @@ class FileModule(ModuleInterface):
 archievenlijst = []
 
 # uitlezen van de zip archieven, deze vervolgens appenden aan de archievenlijst
-def zips_uitlezen():
+def archieven_uitlezen():
     zips = zipfile.ZipFile("ziparchief.zip",'r')
    # print(zips.namelist)
     for item in zips.namelist():
         archievenlijst.append(item)
     # print(archievenlijst)
-
-# uitlezen van de tar archieven, deze vervolgens appenden aan de archievenlijst
-def tars_uitlezen():
-    tars = tarfile.open("tararchief.tar.gz", "r:gz")
-    for tarinfo in tars:
-        archievenlijst.append(tarinfo.name)
-    # print(archievenlijst)
+        tars = tarfile.open("tararchief.tar.gz", "r:gz")
+        for tarinfo in tars:
+            archievenlijst.append(tarinfo.name)
 
 # Alle bestandsnamen wegschrijven naar een .csv op een nieuwe regel.
 def archieflijst_wegschrijven():
     with open("archiefbestanden.csv", "w") as f:
         writer = csv.writer(f, delimiter="\n")
         writer.writerow(archievenlijst)
+
 # taalbepalen met langcheck, file openen -> lezen en testen
 def langcheck():
-    file = open('taalbestand2DE.txt', 'r')
+    file = open('taaltekstbestand.txt', 'r')
     text = file.read()
     file.close()
     test = detect(text)
     print(test)
 
-
-
+# bestandsextensies uit paden halen en wegschrijven
+def extensies_uit_paden():
+    extensiepadlijst = []
+extensiepadlijst  = [os.path.join(r,file) for r,d,f in os.walk("C:\\Users\\Y50\\Desktop\\IPFIT5") for file in f]
+with open("bestandsextensies.csv", "w") as f:
+    writer = csv.writer(f, delimiter="\n")
+    writer.writerow(extensiepadlijst)
 
 
 if __name__ == '__main__':
-    zips_uitlezen()
-    tars_uitlezen()
+    archieven_uitlezen()
     archieflijst_wegschrijven()
+    extensies_uit_paden()
     langcheck()
