5fae51ba881f97f1c13f4042223fcfdce31ad798
Bram Choufoer <bramchoufoer@hotmail.com>
2018-04-11 11:07:27 +0200

    Reset changes in XlsxWriter

--- Utils/XlsxWriter.py
+++ Utils/XlsxWriter.py
@@ -3,7 +3,7 @@ from typing import List
 
 from xlsxwriter import Workbook
 
-# from Utils.Store.Credentials import CredentialStore
+from Utils.Store.Credentials import CredentialStore
 
 
 class XlsxWriter:
@@ -25,10 +25,8 @@ class XlsxWriter:
         config_path = Path(__file__).parent.parent.joinpath('Output')
         Path.mkdir(Path(config_path), exist_ok=True)
 
-        #credentials = CredentialStore()
-        #case = credentials.credential_store.get_state().get('case')
-
-        case = "001"
+        credentials = CredentialStore()
+        case = credentials.credential_store.get_state().get('case')
 
         # Make case folder
         case_path = Path(config_path.joinpath(case))
@@ -36,7 +34,7 @@ class XlsxWriter:
 
         return str(
             case_path.joinpath('{}-{}.xlsx'.format(
-                "001010101011",
+                credentials.time,
                 name.replace(' ', '-')
             ))
         )
