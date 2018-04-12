271d237df99f6e187e532e34abd60a64bcd19151
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-04-12 11:38:57 +0200

    Refactored imports

--- Modules/EmailModule.py
+++ Modules/EmailModule.py
@@ -1,21 +1,21 @@
-import os, os.path
-import pypff
 import csv
-import networkx as nx
-import matplotlib.pyplot as plt
-from collections import defaultdict
+import os, os.path
 import math
-from Interfaces.ModuleInterface import ModuleInterface
-from Utils.ImageHandler import ImageHandler
+import matplotlib.pyplot as plt
+import networkx as nx
 import pandas as pd
-from Utils.Store import Store
-from Models.FileModel import FileModel
+import pypff
+
+from collections import defaultdict
+from Interfaces.ModuleInterface import ModuleInterface
 from io import BytesIO
+from Models.FileModel import FileModel
 from pathlib import Path
+from Utils.ImageHandler import ImageHandler
+from Utils.Store import Store
 
 
 class EmailModuleTest(ModuleInterface):
-
     def __init__(self) -> None:
         super().__init__()
         self.ewf = ImageHandler()
@@ -113,8 +113,8 @@ class EmailModuleTest(ModuleInterface):
 
         fout_path = self.make_path("map_" + folder_name + ".csv")
 
-        with open(fout_path, 'w',  encoding="utf-8" ,newline='') as fout:
-            header = ['from','date','delivered', 'to', 'subject', 'body',
+        with open(fout_path, 'w', encoding="utf-8", newline='') as fout:
+            header = ['from', 'date', 'delivered', 'to', 'subject', 'body',
                       'attachment_count']
 
             for message in message_list:
@@ -252,7 +252,7 @@ class EmailModuleTest(ModuleInterface):
             x for x in os.listdir(self.pathing)
             if x.startswith('verzameling_emailadressen')
         ]
-        with open(os.path.join(self.pathing,'Alle_Email_Adressen.csv'), 'wb') \
+        with open(os.path.join(self.pathing, 'Alle_Email_Adressen.csv'), 'wb') \
                 as out:
             for x in files:
                 f_path = os.path.join(self.pathing, x)
@@ -264,7 +264,7 @@ class EmailModuleTest(ModuleInterface):
 
     def merge_csv_emails(self):
         files = [x for x in os.listdir(self.pathing) if x.startswith('map_')]
-        with open(os.path.join(self.pathing,'Alle_Emails_body_subject.csv'),
+        with open(os.path.join(self.pathing, 'Alle_Emails_body_subject.csv'),
                   'wb') as out:
 
             for x in files:
@@ -279,21 +279,22 @@ class EmailModuleTest(ModuleInterface):
     def merge_csv_email_notes(self):
 
         files = [x for x in os.listdir(self.pathing) if x.startswith('notes')]
-        with open(os.path.join(self.pathing,'Emails_from_to.csv'), 'wb') as out:
+        with open(os.path.join(self.pathing, 'Emails_from_to.csv'),
+                  'wb') as out:
             for x in files:
                 f_path = os.path.join(self.pathing, x)
                 with open(f_path, 'rb') as f:
                     out.write(f.read())
 
         for f in files:
-                os.remove(os.path.join(self.pathing, f))
+            os.remove(os.path.join(self.pathing, f))
 
     def read_from_to(self):
         columns = defaultdict(list)
-        with open(os.path.join(self.pathing,"Emails_from_to.csv")) as f:
+        with open(os.path.join(self.pathing, "Emails_from_to.csv")) as f:
             reader = csv.DictReader(f)
             for row in reader:
-                for(k, v) in row.items():
+                for (k, v) in row.items():
                     if v != k:
                         columns[k].append(v)
 
@@ -301,7 +302,7 @@ class EmailModuleTest(ModuleInterface):
         for line in columns["from"]:
             for word in line.split(' '):
                 if '@' in word:
-                    replaced = word.replace('<', '').replace('>', '')\
+                    replaced = word.replace('<', '').replace('>', '') \
                         .replace(r'"', '').strip()
                     froms.append(replaced)
                     break
@@ -309,7 +310,7 @@ class EmailModuleTest(ModuleInterface):
         for x in columns["to"]:
             for d in x.split(' '):
                 if '@' in d:
-                    replaced2 = d.replace('<', '').replace('>', '').\
+                    replaced2 = d.replace('<', '').replace('>', ''). \
                         replace(r'"', '').strip()
                     tos.append(replaced2)
                     break
@@ -324,12 +325,11 @@ class EmailModuleTest(ModuleInterface):
             csv_fout = csv.DictWriter(fout, fieldnames=header)
             csv_fout.writeheader()
             for froms_to in combined:
-
                 csv_fout.writerow(froms_to)
 
     def graph(self):
         fig = plt.figure(figsize=(40, 15))
-        with open(os.path.join(self.pathing,"Gegevens_Graaf.csv"), 'rt') as f:
+        with open(os.path.join(self.pathing, "Gegevens_Graaf.csv"), 'rt') as f:
             f = csv.reader(f)
             headers = next(f)
             emails = [row for row in f]
@@ -368,14 +368,14 @@ class EmailModuleTest(ModuleInterface):
         nx.draw_networkx_labels(G, pos, font_size=14)
 
         plt.axis('off')
-        plt.savefig(os.path.join(self.pathing,"Graaf.png"), dpi=300)
+        plt.savefig(os.path.join(self.pathing, "Graaf.png"), dpi=300)
 
         # print("Alle CSV bestanden naar XLSX converten.......")
         # TODO: Implement logger
 
     def convert_to_xls(self):
 
-        filename = os.path.join(self.pathing,'Alle_Emails_body_subject.xlsx')
+        filename = os.path.join(self.pathing, 'Alle_Emails_body_subject.xlsx')
         with open(filename, 'w'):
             df_new = pd.read_csv(os.path.join(self.pathing,
                                               'Alle_Emails_body_subject.csv'))
@@ -383,15 +383,15 @@ class EmailModuleTest(ModuleInterface):
             df_new.to_excel(writer, index=False)
             writer.save()
 
-        filename_three = os.path.join(self.pathing,'Emails_from_to.xlsx')
+        filename_three = os.path.join(self.pathing, 'Emails_from_to.xlsx')
         with open(filename_three, 'w', encoding="utf-8"):
             df_new = pd.read_csv(
-                os.path.join(self.pathing,'Emails_from_to.csv'))
+                os.path.join(self.pathing, 'Emails_from_to.csv'))
             writer = pd.ExcelWriter(filename_three)
             df_new.to_excel(writer, index=False)
             writer.save()
 
-        filename_four = os.path.join(self.pathing,'map_Verwijderde items.xlsx')
+        filename_four = os.path.join(self.pathing, 'map_Verwijderde items.xlsx')
         with open(filename_four, 'w', encoding="utf-8"):
             df_new = pd.read_csv(os.path.join(self.pathing,
                                               'map_Verwijderde items.csv'))
@@ -399,10 +399,10 @@ class EmailModuleTest(ModuleInterface):
             df_new.to_excel(writer, index=False)
             writer.save()
 
-        filenaming = os.path.join(self.pathing,"Alle Emailadressen.xlsx")
+        filenaming = os.path.join(self.pathing, "Alle Emailadressen.xlsx")
         with open(filenaming, 'w'):
             df_new = pd.read_csv(
-                os.path.join(self.pathing,'Gegevens_Graaf.csv'))
+                os.path.join(self.pathing, 'Gegevens_Graaf.csv'))
             writer = pd.ExcelWriter(filenaming)
             df_new.to_excel(writer, index=False)
             writer.save()
@@ -415,7 +415,6 @@ class EmailModuleTest(ModuleInterface):
 
 
 if __name__ == "__main__":
-
     store = Store()
     store.image_store.dispatch(
         {
